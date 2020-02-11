297709a76c76283a7aa073d4edd7a0d9f758f685
nloum <39776302+nloum@users.noreply.github.com>
2020-02-10 22:17:04 -0600

    Add comments

--- SimpleMonads/IMaybe.cs
+++ SimpleMonads/IMaybe.cs
@@ -1,10 +1,18 @@
 namespace SimpleMonads
 {
+    /// <summary>
+    /// Represents either a value of type T or nothing at all.
+    /// </summary>
+    /// <typeparam name="T">The type of value that this maybe may contain</typeparam>
     public interface IMaybe<out T> : IMonad<T>, IMaybe
     {
         T Value { get; }
     }
 
+    /// <summary>
+    /// A non-generic maybe useful for code that has to deal with maybes that aren't
+    /// generic.
+    /// </summary>
     public interface IMaybe
     {
         bool HasValue { get; }

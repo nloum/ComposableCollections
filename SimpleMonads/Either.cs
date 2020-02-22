17cb8ca350e56b9ab4f5b14fe266a6215813f98c
nloum <39776302+nloum@users.noreply.github.com>
2020-02-22 10:17:47 -0600

    Updates

--- SimpleMonads/Either.cs
+++ SimpleMonads/Either.cs
@@ -3,6 +3,56 @@ using System.Runtime.Serialization;
 
 namespace SimpleMonads
 {
+    public sealed class Either<T1, T2, T3, T4> : IEither<T1, T2, T3, T4>
+    {
+        public Either(T1 item1)
+        {
+            Item1 = item1.ToMaybe();
+        }
+
+        public Either(T2 item2)
+        {
+            Item2 = item2.ToMaybe();
+        }
+
+        public Either(T3 item3)
+        {
+            Item3 = item3.ToMaybe();
+        }
+
+        public Either(T4 item4)
+        {
+            Item4 = item4.ToMaybe();
+        }
+
+        public IMaybe<T1> Item1 { get; } = Utility.Nothing<T1>();
+        public IMaybe<T2> Item2 { get; } = Utility.Nothing<T2>();
+        public IMaybe<T3> Item3 { get; } = Utility.Nothing<T3>();
+        public IMaybe<T4> Item4 { get; } = Utility.Nothing<T4>();
+    }
+
+    public sealed class Either<T1, T2, T3> : IEither<T1, T2, T3>
+    {
+        public Either(T1 item1)
+        {
+            Item1 = item1.ToMaybe();
+        }
+
+        public Either(T2 item2)
+        {
+            Item2 = item2.ToMaybe();
+        }
+
+        public Either(T3 item3)
+        {
+            Item3 = item3.ToMaybe();
+        }
+
+        public IMaybe<T1> Item1 { get; } = Utility.Nothing<T1>();
+        public IMaybe<T2> Item2 { get; } = Utility.Nothing<T2>();
+        public IMaybe<T3> Item3 { get; } = Utility.Nothing<T3>();
+    }
+
     public static class Either
     {
         public static IEither<T3, T4> Select<T1, T2, T3, T4>(this IEither<T1, T2> source, Func<T1, T3> selector1,
@@ -42,7 +92,8 @@ namespace SimpleMonads
             }
         }
 
-        public static IEither<T3, T2> SelectItem1<T1, T2, T3>(this IEither<T1, T2> source, Func<T1, IEither<T3, T2>> selector1)
+        public static IEither<T3, T2> SelectItem1<T1, T2, T3>(this IEither<T1, T2> source,
+            Func<T1, IEither<T3, T2>> selector1)
         {
             if (source.Item1.HasValue)
             {
@@ -54,7 +105,8 @@ namespace SimpleMonads
             }
         }
 
-        public static IEither<T1, T3> SelectItem2<T1, T2, T3>(this IEither<T1, T2> source, Func<T2, IEither<T1, T3>> selector2)
+        public static IEither<T1, T3> SelectItem2<T1, T2, T3>(this IEither<T1, T2> source,
+            Func<T2, IEither<T1, T3>> selector2)
         {
             if (source.Item1.HasValue)
             {
@@ -110,10 +162,8 @@ namespace SimpleMonads
             Item2 = Utility.Something(item2);
         }
 
-        [DataMember]
-        public IMaybe<T1> Item1 { get; set; }
+        [DataMember] public IMaybe<T1> Item1 { get; set; }
 
-        [DataMember]
-        public IMaybe<T2> Item2 { get; set; }
+        [DataMember] public IMaybe<T2> Item2 { get; set; }
     }
 }
--- SimpleMonads/IEither.cs
+++ SimpleMonads/IEither.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -8,5 +7,20 @@ namespace SimpleMonads
     {
         IMaybe<T1> Item1 { get; }
         IMaybe<T2> Item2 { get; }
+    }
+
+    public interface IEither<out T1, out T2, out T3>
+    {
+        IMaybe<T1> Item1 { get; }
+        IMaybe<T2> Item2 { get; }
+        IMaybe<T3> Item3 { get; }
+    }
+
+    public interface IEither<out T1, out T2, out T3, out T4>
+    {
+        IMaybe<T1> Item1 { get; }
+        IMaybe<T2> Item2 { get; }
+        IMaybe<T3> Item3 { get; }
+        IMaybe<T4> Item4 { get; }
     }
 }

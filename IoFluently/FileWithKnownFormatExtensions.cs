2c14f75cc851b79be43592b338d4b44020a44a48
nloum <39776302+nloum@users.noreply.github.com>
2020-05-08 05:21:53 -0500

    Add more AsFile extension method variants

--- IoFluently/FileWithKnownFormatExtensions.cs
+++ IoFluently/FileWithKnownFormatExtensions.cs
@@ -1,18 +1,73 @@
+using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace IoFluently
 {
     public static class FileWithKnownFormatExtensions
     {
+        public static FileWithKnownFormatSync<T> AsFile<T>(this AbsolutePath path, Func<AbsolutePath, T> read)
+        {
+            return new FileWithKnownFormatSync<T>(path, read);
+        }
+
+        public static FileWithKnownFormatAsync<T> AsFile<T>(this AbsolutePath path, Func<AbsolutePath, Task<T>> read)
+        {
+            return new FileWithKnownFormatAsync<T>(path, read);
+        }
+        
+        public static FileWithKnownFormatSync<T, T> AsFile<T>(this AbsolutePath path, Func<AbsolutePath, T> read,
+            Action<AbsolutePath, T> write)
+        {
+            return new FileWithKnownFormatSync<T, T>(path, read, write);
+        }
+
+        public static FileWithKnownFormatSyncAsync<T, T> AsFile<T>(this AbsolutePath path, Func<AbsolutePath, T> read,
+            Func<AbsolutePath, T, Task> write)
+        {
+            return new FileWithKnownFormatSyncAsync<T, T>(path, read, write);
+        }
+
+        public static FileWithKnownFormatAsyncSync<T, T> AsFile<T>(this AbsolutePath path, Func<AbsolutePath, Task<T>> read,
+            Action<AbsolutePath, T> write)
+        {
+            return new FileWithKnownFormatAsyncSync<T, T>(path, read, write);
+        }
+
+        public static FileWithKnownFormatAsync<T, T> AsFile<T>(this AbsolutePath path, Func<AbsolutePath, Task<T>> read,
+            Func<AbsolutePath, T, Task> write)
+        {
+            return new FileWithKnownFormatAsync<T, T>(path, read, write);
+        }
+        
+        public static FileWithKnownFormatSync<TReader, TWriter> AsFile<TReader, TWriter>(this AbsolutePath path, Func<AbsolutePath, TReader> read,
+            Action<AbsolutePath, TWriter> write)
+        {
+            return new FileWithKnownFormatSync<TReader, TWriter>(path, read, write);
+        }
+
+        public static FileWithKnownFormatSyncAsync<TReader, TWriter> AsFile<TReader, TWriter>(this AbsolutePath path, Func<AbsolutePath, TReader> read,
+            Func<AbsolutePath, TWriter, Task> write)
+        {
+            return new FileWithKnownFormatSyncAsync<TReader, TWriter>(path, read, write);
+        }
+
+        public static FileWithKnownFormatAsyncSync<TReader, TWriter> AsFile<TReader, TWriter>(this AbsolutePath path, Func<AbsolutePath, Task<TReader>> read,
+            Action<AbsolutePath, TWriter> write)
+        {
+            return new FileWithKnownFormatAsyncSync<TReader, TWriter>(path, read, write);
+        }
+
+        public static FileWithKnownFormatAsync<TReader, TWriter> AsFile<TReader, TWriter>(this AbsolutePath path, Func<AbsolutePath, Task<TReader>> read,
+            Func<AbsolutePath, TWriter, Task> write)
+        {
+            return new FileWithKnownFormatAsync<TReader, TWriter>(path, read, write);
+        }
+
         public static FileWithKnownFormatSync<Text, Text> AsTextFile(this AbsolutePath path)
         {
-            return new FileWithKnownFormatSync<Text, Text>(path, absPath =>
-            {
-                return new Text(absPath.ReadLines());
-            }, (absPath, text) =>
-            {
-                absPath.WriteAllLines(text.Lines);
-            });
+            return path.AsFile(absPath => new Text(absPath.ReadLines()),
+                (absPath, text) => absPath.WriteAllLines(text.Lines));
         }
 
         public static Text AsText(this IEnumerable<string> lines)

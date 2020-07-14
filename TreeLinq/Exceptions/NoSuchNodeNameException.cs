3390f176a9177da52a2d6a225b4f5783b0578304
nloum <nathan.loum@bluecats.com>
2020-07-14 15:51:44 -0500

    Setup nuke.build

--- TreeLinq/Exceptions/NoSuchNodeNameException.cs
+++
@@ -1,8 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TreeLinq.Exceptions {
-	public class NoSuchNodeNameException : Exception {
-	}
-}
--- TreeLinq/Exceptions/WrongTypeException.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TreeLinq.Exceptions {
-	public class WrongTypeException : Exception {
-
-	}
-}

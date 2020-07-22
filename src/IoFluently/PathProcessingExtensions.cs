3bb42c6885ef6dba241947397dddf47112234aa0
Nate Loum <39776302+nloum@users.noreply.github.com>
2020-07-22 04:50:21 -0500

    Fix compile error

--- src/IoFluently/PathProcessingExtensions.cs
+++ src/IoFluently/PathProcessingExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Serialization;
 using UtilityDisposables;
 
 namespace IoFluently

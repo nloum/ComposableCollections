17e884c21172d947ba7fa82ae9307750218e1ff0
nloum <nathan.loum@bluecats.com>
2020-07-28 14:35:07 -0500

    Make the delegate classes easier to use

--- src/MoreCollections/DelegateReadOnlyDictionaryEx.cs
+++ src/MoreCollections/DelegateReadOnlyDictionaryEx.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -5,15 +6,28 @@ namespace MoreCollections
 {
     public class DelegateReadOnlyDictionaryEx<TKey, TValue> : ReadOnlyDictionaryBaseEx<TKey, TValue>
     {
-        private IReadOnlyDictionaryEx<TKey, TValue> _wrapped;
+        private IReadOnlyDictionaryEx<TKey, TValue>? _wrapped;
 
         public DelegateReadOnlyDictionaryEx(IReadOnlyDictionaryEx<TKey, TValue> wrapped)
         {
             _wrapped = wrapped;
         }
 
+        protected DelegateReadOnlyDictionaryEx()
+        {
+        }
+
+        protected void SetWrapped(IReadOnlyDictionaryEx<TKey, TValue> wrapped)
+        {
+            _wrapped = wrapped;
+        }
+
         public override bool TryGetValue(TKey key, out TValue value)
         {
+            if (_wrapped == null)
+            {
+                throw new InvalidOperationException("Must called SetWrapped or pass the wrapped value in via constructor before performing any calls on the dictionary");
+            }
             var result = _wrapped.TryGetValue(key);
             value = result.ValueOrDefault;
             return result.HasValue;
@@ -21,12 +35,48 @@ namespace MoreCollections
 
         public override IEnumerator<IKeyValuePair<TKey, TValue>> GetEnumerator()
         {
+            if (_wrapped == null)
+            {
+                throw new InvalidOperationException("Must called SetWrapped or pass the wrapped value in via constructor before performing any calls on the dictionary");
+            }
             return _wrapped.Select(kvp => Utility.KeyValuePair<TKey, TValue>(kvp.Key, kvp.Value)).GetEnumerator();
         }
 
-        public override int Count => _wrapped.Count;
+        public override int Count
+        {
+            get
+            {
+                if (_wrapped == null)
+                {
+                    throw new InvalidOperationException("Must called SetWrapped or pass the wrapped value in via constructor before performing any calls on the dictionary");
+                }
+                return _wrapped.Count;
+            }
+        }
+
         public override IEqualityComparer<TKey> Comparer => EqualityComparer<TKey>.Default;
-        public override IEnumerable<TKey> Keys => _wrapped.Keys;
-        public override IEnumerable<TValue> Values => _wrapped.Values;
+        public override IEnumerable<TKey> Keys
+        {
+            get
+            {
+                if (_wrapped == null)
+                {
+                    throw new InvalidOperationException("Must called SetWrapped or pass the wrapped value in via constructor before performing any calls on the dictionary");
+                }
+                return _wrapped.Keys;
+            }
+        }
+
+        public override IEnumerable<TValue> Values
+        {
+            get
+            {
+                if (_wrapped == null)
+                {
+                    throw new InvalidOperationException("Must called SetWrapped or pass the wrapped value in via constructor before performing any calls on the dictionary");
+                }
+                return _wrapped.Values;
+            }
+        }
     }
 }

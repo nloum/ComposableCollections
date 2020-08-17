a0174963981b9e20695dd4689dfc01961527e626
Nate Loum <39776302+nloum@users.noreply.github.com>
2020-08-17 06:38:51 -0500

    Remove tests and extensions that are moving to ComposableCollections

--- src/MoreCollections/Extensions.cs
+++ src/MoreCollections/Extensions.cs
@@ -10,50 +10,9 @@ namespace MoreCollections
 {
     public static class Extensions
     {
-        public static DictionaryGetOrDefaultDecorator<TKey, TValue> WithDefaultValue<TKey, TValue>(
-            this IDictionary<TKey, TValue> dictionary, Func<TKey, TValue> defaultValue, bool persist)
-        {
-            return new DictionaryGetOrDefaultDecorator<TKey, TValue>(new SystemDelegateDictionaryEx<TKey, TValue>(dictionary), (TKey key, out IMaybe<TValue> maybeResult, out bool persistForThisValue) =>
-            {
-                maybeResult = defaultValue(key).ToMaybe();
-                persistForThisValue = persist;
-            });
-        }
-        
-        public static DictionaryGetOrDefaultDecorator<TKey, TValue> WithDefaultValue<TKey, TValue>(
-            this IDictionary<TKey, TValue> dictionary, GetDefaultValue<TKey, TValue> defaultValue)
-        {
-            return new DictionaryGetOrDefaultDecorator<TKey, TValue>(new SystemDelegateDictionaryEx<TKey, TValue>(dictionary), defaultValue);
-        }
-        
-        public static IReadOnlyList<T> SkipEfficiently<T>(this IReadOnlyList<T> source, int skip)
-        {
-            return new SkipReadOnlyList<T>(source, skip);
-        }
-
-        public static IReadOnlyList<T> TakeEfficiently<T>(this IReadOnlyList<T> source, int take)
-        {
-            return new TakeReadOnlyList<T>(source, take);
-        }
-
-        public static IReadOnlyList<T> TakeEfficiently<T>(this IReadOnlyList<T> source, INumberRange<int> take)
-        {
-            return new TakeReadOnlyList<T>(new SkipReadOnlyList<T>(source, take.LowerBound.ChangeStrictness(false).Value), take.Size);
-        }
-
         public static IEnumerable<T> Take<T>(this IEnumerable<T> source, INumberRange<int> take)
         {
             return source.Skip(take.LowerBound.ChangeStrictness(false).Value).Take(take.Size);
-        }
-
-        public static IReadOnlyList<TOutput> SelectEfficiently<TInput, TOutput>(this IReadOnlyList<TInput> source, Func<TInput, int, TOutput> selector)
-        {
-            return new SelectReadOnlyList<TInput, TOutput>(source, selector);
-        }
-
-        public static IReadOnlyList<TOutput> SelectEfficiently<TInput, TOutput>(this IReadOnlyList<TInput> source, Func<TInput, TOutput> selector)
-        {
-            return new SelectReadOnlyList<TInput, TOutput>(source, (item, index) => selector(item));
         }
 
         public static string GetCommonBeginning(this string str1, string str2)
@@ -87,14 +46,6 @@ namespace MoreCollections
                 removedAll = removedAll && dictionary.Remove(key);
             }
             return removedAll;
-        }
-        
-        /// <summary>
-        /// Converts to a normal .NET KeyValuePair object.
-        /// </summary>
-        public static KeyValuePair<TKey, TValue> ToKeyValuePair<TKey, TValue>(this IKeyValuePair<TKey, TValue> source)
-        {
-            return new KeyValuePair<TKey, TValue>(source.Key, source.Value);
         }
 
         /// <summary>

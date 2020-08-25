d3b48fc0da9309e012acab9aa0cdb9505122b9b0
nloum <39776302+nloum@users.noreply.github.com>
2020-08-25 08:36:09 -0500

    Add rocketseat docs site

---
+++ src/ComposableCollections.Tests/Dictionary/Decorators/ReadWriteLockDictionaryDecoratorTests.cs
@@ -0,0 +1,29 @@
+using System;
+using ComposableCollections.Dictionary.Decorators;
+using ComposableCollections.Dictionary.Sources;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ComposableCollections.Tests.Decorators
+{
+    [TestClass]
+    public class ReadWriteLockDictionaryDecoratorTests
+    {
+        [TestMethod]
+        public void ReadWriteOperationsShouldWorkWithConstructorSyntax()
+        {
+            var underlyingDictionary = new ComposableDictionary<int, string>();
+            var readWriteLockDictionaryDecorator = new ReadWriteLockDictionaryDecorator<int, string>(underlyingDictionary);
+            readWriteLockDictionaryDecorator.Add(3, "Hey");
+            Console.WriteLine(readWriteLockDictionaryDecorator[3]);
+        }
+        
+        [TestMethod]
+        public void ReadWriteOperationsShouldWorkWithFluentSyntax()
+        {
+            var readWriteLockDictionaryDecorator = new ComposableDictionary<int, string>()
+                .WithReadWriteLock();
+            readWriteLockDictionaryDecorator.Add(3, "Hey");
+            Console.WriteLine(readWriteLockDictionaryDecorator[3]);
+        }
+    }
+}

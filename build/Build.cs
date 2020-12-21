ca14826ff46b4dac82fb252b70be34237b1cfa4c
Nate <39776302+nloum@users.noreply.github.com>
2020-12-21 10:29:12 -0600

    Code changes for nuke upgrade

--- build/Build.cs
+++ build/Build.cs
@@ -158,7 +158,7 @@ class Build : NukeBuild
 	        //var settings = new GitVersionSettings().SetToolPath( package.Directory / "tools/netcoreapp3.1/any/gitversion.dll");
 
 	        ReportGenerator(_ => _
-	            .SetToolPath(package.Directory / "tools/netcoreapp3.1/any/reportgenerator.dll")
+	            .SetProcessToolPath(package.Directory / "tools/netcoreapp3.1/any/reportgenerator.dll")
                 .SetReports(TestResultDirectory / "*.xml")
                 .SetReportTypes(ReportTypes.HtmlInline)
                 .SetTargetDirectory(CoverageReportDirectory)
@@ -215,7 +215,7 @@ class Build : NukeBuild
 	    get
 	    {
 		    var package = NuGetPackageResolver.GetGlobalInstalledPackage("GitVersion.Tool", "5.6.0", null);
-		    var settings = new GitVersionSettings().SetToolPath(package.Directory / "tools/net5.0/any/gitversion.dll");
+		    var settings = new GitVersionSettings().SetProcessToolPath(package.Directory / "tools/net5.0/any/gitversion.dll");
 		    var gitVersion = GitVersionTasks.GitVersion(settings).Result;
 		    return gitVersion;
 	    }

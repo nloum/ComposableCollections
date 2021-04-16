0b932c79bf193aadb157590ee9701ac70a008388
Nate <39776302+nloum@users.noreply.github.com>
2021-04-16 18:03:07 -0500

    Fix bug in IFileProvider that sidestepped the OpenReadStream method

--- src/IoFluently.Tests/InMemoryIoServiceTests.cs
+++ src/IoFluently.Tests/InMemoryIoServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using FluentAssertions;
+using Microsoft.Extensions.Configuration;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace IoFluently.Tests
@@ -27,6 +28,27 @@ namespace IoFluently.Tests
             var testTxt = uut.ParseAbsolutePath("/test1.txt");
             testTxt.WriteAllText("testcontents");
             testTxt.ReadAllText().Should().Be("testcontents");
+        }
+
+        [TestMethod]
+        public void FileProviderImplementationShouldWorkForConfiguration()
+        {
+            var configFileContents = $@"{{
+	""ConfigSection1"": {{
+		""Value1"": ""astringvalue""
+	}}
+}}";
+
+            var ioService = new InMemoryIoService( "\n", false, "\\" );
+            ioService.RootFolders.Add( "C:", new InMemoryIoService.Folder() );
+            var root = ioService.ParseAbsolutePath( "C:\\" );
+            ioService.WriteAllText( ioService.ParseAbsolutePath( "C:\\appsettings.json" ), configFileContents );
+
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile( root.Descendants(), "appsettings.json", false, false )
+                .Build();
+
+            configuration["ConfigSection1:Value1"].Should().Be("astringvalue");
         }
     }
 }
--- src/IoFluently/AbsolutePathFileInfoAdapter.cs
+++ src/IoFluently/AbsolutePathFileInfoAdapter.cs
@@ -6,14 +6,19 @@ namespace IoFluently
 {
     public class AbsolutePathFileInfoAdapter : IFileInfo {
         private readonly AbsolutePath _absolutePath;
+        private readonly bool _isPhysicalPath;
 
-        public AbsolutePathFileInfoAdapter( AbsolutePath absolutePath ) => _absolutePath = absolutePath;
+        public AbsolutePathFileInfoAdapter( AbsolutePath absolutePath, bool isPhysicalPath = false )
+        {
+            _absolutePath = absolutePath;
+            _isPhysicalPath = isPhysicalPath;
+        }
 
         public Stream CreateReadStream() => _absolutePath.Open( FileMode.Open, FileAccess.Read, FileShare.Read );
 
         public bool Exists => _absolutePath.Exists();
         public long Length => (long) Math.Round(_absolutePath.FileSize().Bytes);
-        public string PhysicalPath => _absolutePath.ToString();
+        public string PhysicalPath => _isPhysicalPath ? _absolutePath.ToString() : null;
         public string Name => _absolutePath.Name;
         public DateTimeOffset LastModified => _absolutePath.LastWriteTime();
         public bool IsDirectory => _absolutePath.IsFolder();

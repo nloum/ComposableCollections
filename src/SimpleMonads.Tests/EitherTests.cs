5e8fe214747a217d22d9b1ad93c5b6aaa13455b9
Nate <39776302+nloum@users.noreply.github.com>
2021-05-11 14:03:57 -0500

    Add unit tests

--- src/SimpleMonads.Tests/EitherTests.cs
+++ src/SimpleMonads.Tests/EitherTests.cs
@@ -10,7 +10,33 @@ namespace SimpleMonads.Tests
         public void ToStringShouldReturnValidResult()
         {
             var uut = new Either<string, int>("Hi there");
-            uut.ToString().Should().Be("Either<string, int>(string Item1: Hi there)");
+            uut.ToString().Should().Be("Either<Object, string, int>(string Item1: Hi there)");
+        }
+        
+        [TestMethod]
+        public void SubTypesOfShouldAcceptClassHierarchy()
+        {
+            SubTypesOf<Animal>.Either<Dog, Cat> subTypesOf = new Dog();
+            var isDog = false;
+            var isCat = false;
+            subTypesOf.ForEach(dog => isDog = true, cat => isCat = true);
+            isDog.Should().BeTrue();
+            isCat.Should().BeFalse();
+        }
+
+        public class Animal
+        {
+            
+        }
+
+        public class Dog : Animal
+        {
+            
+        }
+
+        public class Cat : Animal
+        {
+            
         }
     }
 }

5cf53bb5958ca29904d0e4a80668562ca1927169
Nate <39776302+nloum@users.noreply.github.com>
2021-05-26 14:11:50 -0500

    Add composable dictionary graphql extension methods for IRequestExecutorBuilder

--- src/ComposableCollections.GraphQL/Extensions.cs
+++ src/ComposableCollections.GraphQL/Extensions.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using ComposableCollections.Dictionary.Interfaces;
 using ComposableCollections.DictionaryWithBuiltInKey.Interfaces;
 using HotChocolate;
+using HotChocolate.Execution.Configuration;
 using HotChocolate.Types;
 using Humanizer;
 using LiveLinq.Dictionary;
@@ -44,6 +45,45 @@ namespace ComposableCollections.GraphQL
             };
         }
 
+        public static IRequestExecutorBuilder AddTopLevelComposableDictionary<TComposableDictionary>(
+            this IRequestExecutorBuilder builder, string name)
+        {
+            var metadata = typeof(TComposableDictionary).GetComposableDictionaryMetadata();
+
+            builder = builder
+                .AddComposableDictionary<TComposableDictionary>(name.Pascalize())
+                .AddTypeExtension(new ObjectTypeExtension(descriptor =>
+                {
+                    descriptor.Name("Query");
+                    descriptor.Field(name.Camelize())
+                        .Type(metadata.QueryObjectType)
+                        .Resolve(rc =>
+                        {
+                            var result = rc.Services.GetRequiredService<TComposableDictionary>();
+                            return result;
+                        });
+                }))
+                .AddTypeExtension(new ObjectTypeExtension(descriptor =>
+                {
+                    descriptor.Name("Mutation");
+                    descriptor.Field(name.Camelize())
+                        .Type(metadata.MutationObjectType)
+                        .Resolve(rc =>
+                        {
+                            var result = rc.Services.GetRequiredService<TComposableDictionary>();
+                            return result;
+                        });
+                }));
+
+            if (metadata.IsObservable)
+            {
+                builder = builder.AddTypeExtension((ObjectTypeExtension) Activator.CreateInstance(metadata.SubscriptionObjectType,
+                    "Subscription", name.Camelize()));
+            }
+
+            return builder;
+        }
+
         public static ISchemaBuilder AddTopLevelComposableDictionary<TComposableDictionary>(
             this ISchemaBuilder builder, string name)
         {
@@ -83,6 +123,22 @@ namespace ComposableCollections.GraphQL
             return builder;
         }
 
+        public static IRequestExecutorBuilder AddComposableDictionary<TComposableDictionary>(this IRequestExecutorBuilder builder, string typeName)
+        {
+            var metadata = typeof(TComposableDictionary).GetComposableDictionaryMetadata();
+
+            builder = builder
+                .AddType((ObjectType)Activator.CreateInstance(metadata.QueryObjectType, typeName + "Query"))
+                .AddType((ObjectType)Activator.CreateInstance(metadata.MutationObjectType, typeName + "Mutation"));
+
+            if (metadata.IsObservable)
+            {
+                builder = builder.AddType((ObjectType)Activator.CreateInstance(metadata.SubscriptionType, typeName + "Change"));
+            }
+            
+            return builder;
+        }
+        
         public static ISchemaBuilder AddComposableDictionary<TComposableDictionary>(this ISchemaBuilder builder, string typeName)
         {
             var metadata = typeof(TComposableDictionary).GetComposableDictionaryMetadata();

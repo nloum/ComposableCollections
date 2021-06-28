a362c0fc647fca8e26d78abc3ae5d97c7db832d6
Nate <39776302+nloum@users.noreply.github.com>
2021-06-28 10:00:16 -0500

    Add more useful exception messages

--- src/CodeIO/LoadedTypes/Read/Extensions.cs
+++ src/CodeIO/LoadedTypes/Read/Extensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Linq;
 using System.Reflection;
+using System.Text;
 using System.Text.RegularExpressions;
 using CodeIO.LoadedTypes.Read;
 using ComposableCollections;
@@ -9,6 +10,148 @@ namespace CodeIO
 {
     public static class Extensions
     {
+        /// <summary>
+	    /// Converts a <see cref="Type"/> to its C# name. E.g., if you pass in typeof(<see cref="IEnumerable{int}"/>) this
+	    /// will return "IEnumerable<int>".
+	    /// This is useful mainly in exception error messages.
+	    /// </summary>
+	    /// <param name="type">The type to get a C# description of.</param>
+	    /// <returns>The string representation of a human-friendly type name</returns>
+	    public static string GetCSharpTypeName( this Type type, Type[] genericParameters, bool includeNamespaces = false ) {
+		    if ( type == typeof( long ) ) {
+			    return "long";
+		    }
+
+		    if ( type == typeof( int ) ) {
+			    return "int";
+		    }
+
+		    if ( type == typeof( short ) ) {
+			    return "short";
+		    }
+
+		    if ( type == typeof( ulong ) ) {
+			    return "ulong";
+		    }
+
+		    if ( type == typeof( uint ) ) {
+			    return "uint";
+		    }
+
+		    if ( type == typeof( ushort ) ) {
+			    return "ushort";
+		    }
+
+		    if ( type == typeof( byte ) ) {
+			    return "byte";
+		    }
+
+		    if ( type == typeof( sbyte ) ) {
+			    return "sbyte";
+		    }
+
+		    if ( type == typeof( string ) ) {
+			    return "string";
+		    }
+
+		    if ( type == typeof( void ) ) {
+			    return "void";
+		    }
+
+			var baseName = new StringBuilder();
+			var typeName = includeNamespaces ? type.Namespace + "." + type.Name : type.Name;
+		    if ( type.Name.Contains( "`" ) ) {
+			    baseName.Append( typeName.Substring( 0, typeName.IndexOf( "`" ) ) );
+		    } else {
+			    baseName.Append( typeName );
+		    }
+
+		    if ( genericParameters.Any() ) {
+			    baseName.Append( "<" );
+			    for ( var i = 0; i < genericParameters.Length; i++ ) {
+				    baseName.Append( GetCSharpTypeName( genericParameters[i], includeNamespaces ) );
+				    if ( i + 1 < genericParameters.Length ) {
+					    baseName.Append( ", " );
+				    }
+			    }
+
+			    baseName.Append( ">" );
+		    }
+
+		    return baseName.ToString();
+	    }
+
+		/// <summary>
+		/// Converts a <see cref="Type"/> to its C# name. E.g., if you pass in typeof(<see cref="IEnumerable{int}"/>) this
+		/// will return "IEnumerable<int>".
+		/// This is useful mainly in exception error messages.
+		/// </summary>
+		/// <param name="type">The type to get a C# description of.</param>
+		/// <returns>The string representation of a human-friendly type name</returns>
+		public static string GetCSharpTypeName( this Type type, bool includeNamespaces = false ) {
+		    if ( type == typeof(long) ) {
+			    return "long";
+		    }
+
+		    if ( type == typeof( int ) ) {
+			    return "int";
+		    }
+
+			if ( type == typeof(short) ) {
+			    return "short";
+		    }
+
+			if ( type == typeof( ulong ) ) {
+				return "ulong";
+			}
+
+			if ( type == typeof( uint ) ) {
+				return "uint";
+			}
+
+			if ( type == typeof( ushort ) ) {
+				return "ushort";
+			}
+
+			if ( type == typeof( byte ) ) {
+				return "byte";
+			}
+
+			if ( type == typeof( sbyte ) ) {
+				return "sbyte";
+			}
+
+			if ( type == typeof( string ) ) {
+				return "string";
+			}
+
+			if ( type == typeof( void ) ) {
+				return "void";
+			}
+
+			var baseName = new StringBuilder();
+			var typeName = includeNamespaces ? type.Namespace + "." + type.Name : type.Name;
+			if ( type.Name.Contains( "`" ) ) {
+				baseName.Append( typeName.Substring( 0, typeName.IndexOf( "`" ) ) );
+			} else {
+				baseName.Append( typeName );
+			}
+
+			if ( type.GenericTypeArguments.Any() ) {
+			    baseName.Append( "<" );
+			    for ( var i = 0; i < type.GenericTypeArguments.Length; i++) {
+				    baseName.Append( GetCSharpTypeName( type.GenericTypeArguments[i], includeNamespaces ) );
+				    if ( i + 1 < type.GenericTypeArguments.Length ) {
+					    baseName.Append( ", " );
+				    }
+			    }
+
+			    baseName.Append(">");
+		    }
+
+		    return baseName.ToString();
+	    }
+
         public static TypeReader AddReflection(this TypeReader typeReader)
         {
             return typeReader.AddTypeFormat<Type>(type => type.GetTypeIdentifier(), (type, lazyTypes) =>
@@ -86,7 +229,7 @@ namespace CodeIO
                     });
                 }
 
-                throw new NotImplementedException();
+                throw new NotImplementedException($"Cannot process type: {type.GetCSharpTypeName()}");
             });
         }
         
@@ -209,7 +352,7 @@ namespace CodeIO
                 return Visibility.Public;
             }
 
-            throw new NotImplementedException();
+            throw new NotImplementedException($"Unknown visibility for {t.GetCSharpTypeName()}");
         }
     }
 }

d2c12a6a2cb64c7946569853bd617357f35fe663
Nate <39776302+nloum@users.noreply.github.com>
2021-06-29 22:34:40 -0500

    Remove outdated test

--- src/IoFluently.Tests/ZipIoServiceTests.cs
+++ src/IoFluently.Tests/ZipIoServiceTests.cs
@@ -11,24 +11,6 @@ namespace IoFluently.Tests
     public class ZipIoServiceTests : VerifyBase
     {
         [TestMethod]
-        public void ShouldCreateEmptyZipFile()
-        {
-            var inMemoryIoService = new InMemoryIoService("\n", true, "/", false);
-            inMemoryIoService.RootFolders.Add("/", new InMemoryIoService.Folder());
-            var testZipFilePath = inMemoryIoService.ParseAbsolutePath("/test.zip");
-            var result = testZipFilePath.AsZipFile(true);
-            result.SetTemporaryFolder(result.ParseAbsolutePath("/tmp"));
-
-            using (var stream = result.ZipFilePath.Open(FileMode.Open, FileAccess.Read))
-            {
-                using (var archive = new ZipArchive(stream, ZipArchiveMode.Read))
-                {
-                    archive.Entries.Should().BeEmpty();
-                }
-            }
-        }
-        
-        [TestMethod]
         public void ShouldZipUpSingleFile()
         {
             var inMemoryIoService = new InMemoryIoService("\n", true, "/", false);

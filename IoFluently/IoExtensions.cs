1045381bd59fe9f736f4eed844cd5efb26e8daea
nloum <nathan.loum@bluecats.com>
2021-07-07 15:59:10 -0500

    Improve the Fallback APIs

--- IoFluently/IoExtensions.cs
+++ IoFluently/IoExtensions.cs
@@ -14,10 +14,10 @@ namespace IoFluently
         /// </summary>
         /// <param name="path">The path that temporary changes will be made to.</param>
         /// <returns>An object that, when disposed of, undoes any changes made to the specified path.</returns>
-        public static IDisposable TemporaryChanges(this AbsolutePath path)
+        public static IDisposable TemporaryChanges(this IHasAbsolutePath path)
         {
-            var backupPath = path.IoService.TryWithExtension(path, x => x + ".backup").Value;
-            var translation = path.Translate(backupPath);
+            var backupPath = path.IoService.TryWithExtension(path.Path, x => x + ".backup").Value;
+            var translation = path.Path.Translate(backupPath);
             translation.IoService.Copy(translation, overwrite: true);
 
             return new AnonymousDisposable(() => translation.IoService.Move(translation.Invert(), overwrite: true));
@@ -26,45 +26,24 @@ namespace IoFluently
         /// <summary>
         /// If <see cref="mainPath"/> exists, then return <see cref="mainPath"/>. Otherwise, return <see cref="fallbackPath"/>.
         /// </summary>
-        public static AbsolutePath FallbackTo(this AbsolutePath mainPath, string fallbackPath)
+        public static THasAbsolutePath FallbackTo<THasAbsolutePath>(this THasAbsolutePath mainPath, THasAbsolutePath fallbackPath, bool copy = false)
+            where THasAbsolutePath : IHasAbsolutePath
         {
-            return FallbackTo(mainPath, mainPath.IoService.ParsePathRelativeToDefault(fallbackPath));
-        }
-
-        /// <summary>
-        /// If <see cref="mainPath"/> exists, then return <see cref="mainPath"/>. Otherwise, return <see cref="fallbackPath"/>.
-        /// </summary>
-        public static AbsolutePath FallbackTo(this AbsolutePath mainPath, AbsolutePath fallbackPath)
-        {
-            if (!mainPath.Exists)
+            if (!mainPath.Path.Exists)
             {
-                return fallbackPath;
+                if (copy)
+                {
+                    fallbackPath.Path.Copy(mainPath.Path).Copy();
+                }
+                else
+                {
+                    return fallbackPath;
+                }
             }
 
             return mainPath;
         }
 
-        /// <summary>
-        /// If <see cref="mainPath"/> doesn't exist, then creates it by copying from <see cref="fallbackPath"/>.
-        /// </summary>
-        public static AbsolutePath FallbackCopyFrom(this AbsolutePath mainPath, string fallbackPath)
-        {
-            return FallbackCopyFrom(mainPath, mainPath.IoService.ParsePathRelativeToDefault(fallbackPath));
-        }
-
-        /// <summary>
-        /// If <see cref="mainPath"/> doesn't exist, then creates it by copying from <see cref="fallbackPath"/>.
-        /// </summary>
-        public static AbsolutePath FallbackCopyFrom(this AbsolutePath mainPath, AbsolutePath fallbackPath)
-        {
-            if (!mainPath.Exists)
-            {
-                fallbackPath.Copy(mainPath);
-            }
-
-            return mainPath;
-        }
-    
         /// <summary>
         /// Don't use this method, instead use the / operator on AbsolutePath objects (and related objects) to write
         /// idiomatic code with IoFluently.

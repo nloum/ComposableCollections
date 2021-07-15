57c3a2be724adbd373fabb49c7340f2cf58a5587
nloum <nathan.loum@bluecats.com>
2021-07-15 12:36:41 -0500

    Fix issue where CurrentDirectory was ignored for the IoServiceEnvironmentDecorator

--- IoFluently/IoServiceEnvironmentDecorator.cs
+++ IoFluently/IoServiceEnvironmentDecorator.cs
@@ -4,13 +4,25 @@ namespace IoFluently
 {
     public class IoServiceEnvironmentDecorator : IoServiceDecoratorBase, IIoEnvironmentService
     {
-        public IoServiceEnvironmentDecorator(IIoService decorated) : base(decorated)
+        private readonly IIoEnvironmentService _decorated;
+
+        public IoServiceEnvironmentDecorator(IIoEnvironmentService decorated) : base(decorated)
         {
+            _decorated = decorated;
         }
 
-        public Folder CurrentDirectory { get; set; }
-        public Folder TemporaryFolder { get; set; }
-        
+        public Folder CurrentDirectory
+        {
+            get => _decorated.CurrentDirectory;
+            set => _decorated.CurrentDirectory = value;
+        }
+
+        public Folder TemporaryFolder
+        {
+            get => _decorated.TemporaryFolder;
+            set => _decorated.TemporaryFolder = value;
+        }
+
         public MissingPath GenerateUniqueTemporaryPath(string extension = null)
         {
             return (TemporaryFolder / Guid.NewGuid().ToString()).ExpectMissingPath();
